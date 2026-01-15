dcdf677a422021191c9599a58e365aa64d7c76d0
Mango Meow <mangomeow6@gmail.com>
2026-01-15 05:02:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-250662726-c8695",
+  "appId": "1:919160911783:web:8346b740f7c3909cb7e882",
+  "apiKey": "AIzaSyC2KlWt16pNNHMTP1daCPJWiScLD_JmMsM",
+  "authDomain": "studio-250662726-c8695.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "919160911783"
+};
